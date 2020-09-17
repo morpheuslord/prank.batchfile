08f09875fa07243eca1045d99e1dd1d4bfacf445
morphouslord <70637311+morphouslord@users.noreply.github.com>
2020-09-17 09:45:35 +0530

    Add files via upload

---
+++ prank.bat
@@ -0,0 +1,211 @@
+@echo off
+
+mode con:cols=200 lines=100
+
+echo hey,are u insane[answer only in yes or no],if u dont answre i will self distruct ur system
+set/p insane=
+if %insane%==yes goto insane 
+if %insane%==no goto sane
+:insane
+:header
+ echo u are going to an asylme
+echo  meet u there:]
+ echo now i have permission to extract ur files
+
+
+ echo decrypting files
+ echo ========================
+pause 
+
+echo taking administrator access
+echo =================================
+pause
+
+echo admin access denaied,retreat
+echo ==================================
+pause
+
+echo admin access denaied,retreat
+echo ==================================
+pause
+
+echo admin access denaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access recived
+echo ==================================
+pause
+
+echo extracting windows
+echo ==================================
+pause
+
+echo transfering files
+echo ==================================
+pause
+
+echo receving trasfer permission
+echo ==================================
+pause
+
+echo transfer data process starts
+echo ==================================
+pause
+
+echo retiving data copy 
+echo ==================================
+pause
+
+echo laying down rat
+echo ==================================
+pause
+
+echo establishing remote access
+echo ==================================
+pause
+
+echo thanks for the data,have a nice day
+pause
+exit
+
+
+
+:sane 
+:header
+echo but u are mad
+echo u are hacked
+echo ur pc is done in 20 sec
+echo now ur pc is in my control
+echo decrypting data
+echo ==================================
+pause
+echo extracting windows
+echo ==================================
+pause
+
+echo taking administrator access
+echo ==================================
+pause
+
+echo admin access denaied,retreat
+echo ==================================
+pause
+
+echo admin access denaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access dinaied,retreat
+echo ==================================
+pause
+
+echo admin access recived
+echo ==================================
+pause
+
+echo extracting files
+echo ==================================
+pause
+
+echo extracting videos
+echo ==================================
+pause
+
+echo extracting downloades
+echo ==================================
+pause
+
+echo extracting software
+echo ==================================
+pause
+
+echo extracting remote access
+echo ==================================
+pause
+
+echo establishing remote access
+echo ==================================
+pause
+
+echo establishing backup
+echo ==================================
+pause
+
+echo retriving data
+echo ==================================
+pause
+
+echo establishing rat
+echo ==================================
+pause
+
+echo closing transfer
+echo ==================================
+pause
+echo timeout brother,thanks for the data,have a nice day
+echo ====================================================================
+echo ====================================================================
+timeout 20
+shutdown-s-t 2000
+
